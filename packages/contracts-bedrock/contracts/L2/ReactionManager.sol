ac37fb5d005c4a05d170eaf5d5df307a35f255b5
Namra Patel <namrajpatel@gmail.com>
2023-03-08 19:37:57 -0500

    add owner and calling restrictions to ReactionManager

--- packages/contracts-bedrock/contracts/L2/ReactionManager.sol
+++ packages/contracts-bedrock/contracts/L2/ReactionManager.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract Reactivity {
+import { Predeploys } from "../libraries/Predeploys.sol";
+
+contract ReactionManager {
 
     struct Reaction {
         address contractAddress;
@@ -9,13 +11,30 @@ contract Reactivity {
         bytes parameters;
     }
 
+    address public constant DEPOSITOR_ACCOUNT = 0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001;
+    address owner;
     mapping (address => Reaction) reactions;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updateOwner(address newOwner) public {
+        require(msg.sender == owner, "Only the owner can update the owner");
+        owner = newOwner;
+    }
+
     function registerReaction(address watchedAddress, address contractAddress, string memory functionSignature, bytes memory parameters) public {
+
+        // Check that setter the contractAddress itself
+        require(msg.sender == contractAddress, "Only the called contract itself can register a reaction");
+
         reactions[watchedAddress] = Reaction(contractAddress, functionSignature, parameters);
     }
 
     function react(address watchedAddress) public {
+        require(msg.sender == DEPOSITOR_ACCOUNT, "Only the depositor account can trigger a reaction");
+
         Reaction storage reaction = reactions[watchedAddress];
         require(reaction.contractAddress != address(0), "No reaction registered for this address");
 
